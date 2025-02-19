b4ca96172bbad947584a086ba04d4811d73fbec4
Magenta <avelon4321@gmail.com>
2025-02-19 11:42:37 -0600

    Add species class to fully update to 0.0.1

---
+++ src/main/java/com/github/obsidianoasisspecies/species/Species.java
@@ -0,0 +1,20 @@
+package com.github.obsidianoasisspecies.species;
+
+public enum Species {
+    HUMAN("§7Human"),
+    WEREWOLF("§6Werewolf"),
+    VAMPIRE("§4Vampire"),
+    NIGHTCREATURE("§bNight Creature"),
+    SOULFORGER("§8Soul Forger"),
+    MERFOLK("§9Merfolk");
+
+    private final String name;
+
+    Species(String name) {
+        this.name = name; 
+   }
+
+   public String getName() { 
+        return name; 
+   }
+}
